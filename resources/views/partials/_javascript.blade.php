1573c40ed40015369aa87d037b3ddfab9eb17d91
Agastya Darma <gedeagas22@gmail.com>
2016-09-17 23:39:07 +0700

    Partials Update ( Cleaning )

---
+++ resources/views/partials/_javascript.blade.php
@@ -0,0 +1,6 @@
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
