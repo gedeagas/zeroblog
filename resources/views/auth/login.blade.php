80719d56cbafb75fd58f29600895e9e96e30d901
Agastya Darma <gedeagas22@gmail.com>
2016-09-26 06:56:28 +0700

    Updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,18 @@
 @section('thetitle', 'Login')
 
 @section('konten')
+<div class="login-bg">
 
+	<div class="container-fluid">
+			<div class="col-md-6 col-md-offset-3">
+                <div class="embed-responsive embed-responsive-16by9 ">   
+                    <video id="videoElementID" autoplay loop>
+                      <source src="/video.mp4" type="video/mp4" />
+                      <source src="/video.ogg" type="video/ogg" />
+                      Not Supported....
+                    </video>
+                </div> 
+            </div>
 	<div class="row">
 		<div class="col-md-6 col-md-offset-3">
 			{!! Form::open() !!}
@@ -20,5 +31,7 @@
 			{!! Form::close() !!}
 		</div>
 	</div>
+	</div>
 
+</div>
 @endsection
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -3,15 +3,18 @@
 @section('thetitle', "$post->title")
 
 @section('konten')
+<div class="container-fluid">
 
 <div class="row">
 	<div class="col-md-8 col-md-offset-2 raleway-font">
 		<h1 class="raleway-font">{{$post->title}}</h1>
+		<p class="raleway-bold">Agastya Darma Laksana</p>
+		<p class="raleway-light">CEO Of Archipel Corp</p>
 		<p class="">{!!$post->body!!}</p>
 		<hr>
 		<p>Posted In : {{ $post->category->name }}</p>
 	</div>
 
 </div>
-
+</div>
 @endsection
