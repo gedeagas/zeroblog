11107fb922f234c1e18a1f79c755a92cfb0a16e3
Agastya Darma <gedeagas22@gmail.com>
2016-09-26 12:22:39 +0700

    Nav Bar Fix

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -4,17 +4,29 @@
 
 @section('konten')
 <div class="container-fluid">
-
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+		<ol class="breadcrumb">
+		  <li><a href="#">Home</a></li>
+		  <li><a href="#">{{ $post->category->name }}</a></li>
+		  <li class="active">{{ $post->title }}</li>
+		</ol>
+	</div>
+</div>
 <div class="row">
 	<div class="col-md-8 col-md-offset-2 raleway-font">
 		<h1 class="raleway-font">{{$post->title}}</h1>
-		<p class="raleway-bold"><p class="raleway-light">By, </p>Agastya Darma Laksana</p>
+		<p class="raleway-bold">By, Agastya Darma Laksana</p>
 		
 		<p class="">{!!$post->body!!}</p>
-		<hr>
-		<p>Posted In : {{ $post->category->name }}</p>
+		
+	</div>
+	<div class="col-md-8 col-md-offset-2 raleway-font">
+	<hr>
+	<p>Posted In : {{ $post->category->name }}</p>
 	</div>
 
+	
 </div>
 </div>
 @endsection
